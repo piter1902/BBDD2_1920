0e82c407069d636d64e635e42b5437fe481bf784
Pedro Tamargo <pedro.tamargo.allue@gmail.com>
2020-05-08 13:05:22 +0200

    Ficheros del esquema global conjunto

---
+++ Practica3/enunciado parte 2/global/delete_global.sql
@@ -0,0 +1,9 @@
+-- Drop de las vistas
+drop view Personal_view;
+drop view Medico_view;
+drop view Enfermero_view;
+drop view Paciente_view;
+drop view Asignados_view;
+drop view Atendidos_view;
+drop view Diagnostico_view;
+drop view Ingresos_view;
