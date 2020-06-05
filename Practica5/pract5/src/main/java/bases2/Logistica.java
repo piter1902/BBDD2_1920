e822cb34f2b1643650b3ecfe59c9fa2f2ff969e2
jtambo99 <jtambo@live.com>
2020-06-05 13:16:32 +0200

    Hacemos pimeras pruebas con pasos = 1

--- Practica5/pract5/src/main/java/bases2/Logistica.java
+++ Practica5/pract5/src/main/java/bases2/Logistica.java
@@ -1,4 +1,4 @@
-package practica5;
+package bases2;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -30,98 +30,126 @@ import org.apache.hadoop.hbase.client.Scan;
 public class Logistica {
 	private static final int TOTAL_FEATURES = 1600;
 	private static final double ALPHA = 0.01;
-	private static final int TOTAL_PASOS = 20;
+	// private static final int TOTAL_PASOS = 20;
+	private static final int TOTAL_PASOS = 1;
 	public static double[] thetasAct = new double[TOTAL_FEATURES + 1];
-	
-	public static class LogisticMapper 
-	                      extends TableMapper<LongWritable, DoubleWritable> {
+
+	public static class LogisticMapper extends TableMapper<LongWritable, DoubleWritable> {
 
 		private LongWritable keyout = new LongWritable();
 		private DoubleWritable valout = new DoubleWritable();
-		
+
 		@Override
-		public void map(ImmutableBytesWritable key, Result value, Context context) throws IOException, InterruptedException {			
-			//key=identificador de cliente 
-			//value=toda la informacion asociada al cliente: clasificación A o B, caracteristicas no nulas
-			
-			//obtener un diccionario para la familia de columnas car: calificador_columna->valor
+		public void map(ImmutableBytesWritable key, Result value, Context context)
+				throws IOException, InterruptedException {
+			// key=identificador de cliente
+			// value=toda la informacion asociada al cliente: clasificación A o B,
+			// caracteristicas no nulas
+
+			// obtener un diccionario para la familia de columnas car:
+			// calificador_columna->valor
 			NavigableMap<byte[], byte[]> mapCar = value.getFamilyMap(Bytes.toBytes("car"));
-			
-			//para el cliente key, crear un vector xi donde se almacenarán sus 1 + 1600 caracteristicas
+
+			// para el cliente key, crear un vector xi donde se almacenarán sus 1 + 1600
+			// caracteristicas
 			double[] xi = new double[thetasAct.length];
-			//caracteristica 0 siempre es igual a 1
+			// caracteristica 0 siempre es igual a 1
 			xi[0] = 1;
-			
-			//Con los datos de mapCar, rellenar en xi las caracteristicas
-			for(byte[] i : mapCar.keySet()){
+
+			// De esta manera nos aseguramos de que se ponen a 0
+			for (int i = 1; i < xi.length; i++) {
+				xi[i] = 0;
+			}
+
+			// Con los datos de mapCar, rellenar en xi las caracteristicas
+			for (byte[] i : mapCar.keySet()) {
 				xi[Integer.parseInt(Bytes.toString(i))] = 1.0;
 			}
-			
-			//Hallar producto escalar thetasAct*xi
-			for(int i=0;i<xi.length;i++){
-				z +=  thetasAct[i]* xi[i];
-			}
-			//leer de parametro value, clasificacion A o B de cliente
-			String classi = Bytes.toString(value.getValue(Bytes.toBytes("cli"), Bytes.toBytes("class")));
-			double yi = classi.equals("A")?0.0:1.0;
-			
-			//emitir para cada dimenson j el valor correspondiente a
-			//uno de los sumandos del sumatorio del algoritmo del enunciado
-			
-		}
-		
-		
-		public static class LogisticReducer
-	       extends Reducer<LongWritable,DoubleWritable,LongWritable,DoubleWritable> {
-	    
 
-	    public void reduce(LongWritable key, Iterable<DoubleWritable> values, Context context) 
-	    		     throws IOException, InterruptedException {
-		//codigo de reducer
-	    }
-	  }
+			double prod_Esc = 0.0;
+
+			// Hallar producto escalar thetasAct*xi
+			for (int i = 0; i < xi.length; i++) {
+				prod_Esc += thetasAct[i] * xi[i];
+			}
+
+			// leer de parametro value, clasificacion A o B de cliente
+			String classi = Bytes.toString(value.getValue(Bytes.toBytes("cli"), Bytes.toBytes("class")));
+			double yi = classi.equals("A") ? 0.0 : 1.0;
+
+			double g = 0.0;
+
+			g = (1 / 1 + Math.exp(-prod_Esc));
+
+			g = yi - g;
+
+			// emitir para cada dimenson j el valor correspondiente a
+			// uno de los sumandos del sumatorio del algoritmo del enunciado
+			for (int j = 0; j < TOTAL_FEATURES + 1; j++) {
+				context.write(new LongWritable(j), new DoubleWritable(g * xi[j]));
+			}
+		}
+
+		public static class LogisticReducer
+				extends Reducer<LongWritable, DoubleWritable, LongWritable, DoubleWritable> {
+
+			public void reduce(LongWritable key, Iterable<DoubleWritable> values, Context context)
+					throws IOException, InterruptedException {
+
+				double sum = 0;
+				for (DoubleWritable value : values) {
+					sum += value.get();
+				}
+				try {
+					// Se escribira en el fichero el par <j,'sumaDeValoresj'>
+					context.write(key, new DoubleWritable(sum));
+				} catch (IOException | InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
-	
-	
-	
 
 	public static void main(String[] args) {
 		Configuration conf = HBaseConfiguration.create();
+
 		FileSystem fs = FileSystem.get(conf);
-		for (int i=0: i < thetasAct.lenght; i++){
+		for (int i = 0; i < thetasAct.length; i++) {
 			thetasAct[i] = 0.0;
 		}
-		for (int paso = 0; paso < TOTAL_PASOS; paso++) {		
-			//preparar el trabajo map/reduce para calcular el gradiente
-			Job job = Job.getInstance(conf,"Regresion logistica");
+
+		for (int paso = 0; paso < TOTAL_PASOS; paso++) {
+			// preparar el trabajo map/reduce para calcular el gradiente
+			Job job = Job.getInstance(conf, "Regresion logistica");
 
 			FileOutputFormat.setOutputPath(job, new Path("out"));
-			
-			//lanzar y esperar a que finalice el trabajo
+
+			// lanzar y esperar a que finalice el trabajo
 			boolean exito = job.waitForCompletion(true);
-			//si el trabajo finalizo sin exito, acabar
-			if (!exito) System.exit(-1);
-			
-			//una vez finalizado el trabajo que calcula el gradiente,
-			//leer de los ficheros el gradiente calculado
-			
-			//actualizar los thetasAct
-			thetasAct = actualizarThetas(thetasAct, grad, ALPHA);
-			
-			//borrar directorio out, preparando el siguiente paso
+			// si el trabajo finalizo sin exito, acabar
+			if (!exito)
+				System.exit(-1);
+
+			// una vez finalizado el trabajo que calcula el gradiente,
+			// leer de los ficheros el gradiente calculado
+
+			// actualizar los thetasAct
+			// thetasAct = actualizarThetas(thetasAct, grad, ALPHA);
+
+			// borrar directorio out, preparando el siguiente paso
 			fs.delete(new Path("out"), true);
-		} 
-		
-		//Guardar a fichero los thetasAct
+		}
+
+		// Guardar a fichero los thetasAct
 		guardar(thetasAct, "thetas.csv");
-		
+
 	}
-	
+
 	private static void guardar(double[] thetas, String nomFich) throws IOException {
 		PrintWriter out = new PrintWriter(nomFich);
 		out.print(Double.toString(thetas[0]));
 		for (int i = 1; i < thetas.length; i++) {
-			out.print(","+ Double.toString(thetas[i]));
+			out.print("," + Double.toString(thetas[i]));
 		}
 		out.close();
 	}
