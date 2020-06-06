88b79b3d98379ffb1205a05f932c9701a1f3520a
Pedro Tamargo <pedro.tamargo.allue@gmail.com>
2020-06-06 16:43:36 +0200

    La pusheooo

--- Practica5/pract5/src/main/java/bases2/Validacion.java
+++ Practica5/pract5/src/main/java/bases2/Validacion.java
@@ -6,9 +6,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Validacion {
-	
-	
-	
+
 	public static double[] leerThetas(String path) {
 		double[] values = null;
 		try {
@@ -20,61 +18,66 @@ public class Validacion {
 				values[i] = Double.parseDouble(toks[i]);
 			}
 			scan.close();
-			
+
 		} catch (FileNotFoundException e) {
 			System.err.println("Error: fichero no existe " + path);
 		}
 		return values;
 	}
-	
+
 	public static void main(String[] args) throws IOException {
-		//leer thetas de fichero
+		// leer thetas de fichero
 		double[] thetas = leerThetas("thetas.csv");
-		
-		//para cada cliente del fichero, leer caracteristicas xi y clasificacion real yi 
-		//calcular clasificacion predicha segun thetas: yiPredicha = 1/(1 + exp(-xi*thetas)>0.5
-		//calcular datos de la matriz de confusion
+
+		// para cada cliente del fichero, leer caracteristicas xi y clasificacion real
+		// yi
+		// calcular clasificacion predicha segun thetas: yiPredicha = 1/(1 +
+		// exp(-xi*thetas)>0.5
+		// calcular datos de la matriz de confusion
 		String datos = "dataTest.csv";
-		try{
+		try {
 			Scanner scanner = new Scanner(new File(datos));
 			String line = null;
-			int apos = 0;  int bfp = 0;
-			int afp = 0;  int bpos = 0;
+			int apos = 0;
+			int bfp = 0;
+			int afp = 0;
+			int bpos = 0;
 
-			while ( scanner.hasNextLine() ){
+			while (scanner.hasNextLine()) {
 				line = scanner.nextLine();
 				String[] elems = line.split(",");
-				String class = elems[1];
+				String clase = elems[1];
 
 				double prod = thetas[0]; // pq la primera es un 1 siempre
-				for (int i=2; i < elems.length; i++){
-					prod += elems[i]*thetas[i-1];
+				for (int i = 2; i < elems.length; i++) {
+					prod += Double.parseDouble(elems[i]) * thetas[i - 1];
 				}
-				double g = (1 / (1+ Math.exp(-prod)));
-				if ( class.equals("A")){
-					//letra es A
-					if ( g < 0.5) 
-						//modelo predice A --> apos
+				double g = (1 / (1 + Math.exp(-prod)));
+				if (clase.equals("A")) {
+					// letra es A
+					if (g < 0.5)
+						// modelo predice A --> apos
 						apos++;
-					else bfp++; // modelo predice B y es A --> bfp
-				}
-				else {
-					//letra es B
-					if ( g < 0.5)
-						//modelo predice A --> afp
+					else
+						bfp++; // modelo predice B y es A --> bfp
+				} else {
+					// letra es B
+					if (g < 0.5)
+						// modelo predice A --> afp
 						afp++;
-					else bpos++; // modelo predice B y es B --> bpos
+					else
+						bpos++; // modelo predice B y es B --> bpos
 				}
 			}
 			scanner.close();
-		}catch (FileNotFoundException e){
-			System.err.println("Error: fichero no existe " + path);
+			// mostrar matriz de confusion y porcentaje de aciertos
+			System.out.println("\tA\t\tB");
+			System.out.println("A\t" + apos + "\t\t" + bfp);
+			System.out.println("B\t" + afp + "\t\t" + bpos);
+		} catch (FileNotFoundException e) {
+			System.err.println("Error: fichero no existe " + datos);
 		}
-		//mostrar matriz de confusion y porcentaje de aciertos
-		System.out.println("\tA\t\tB");
-		System.out.println("A\t"+apos+"\t\t"+bfp);
-		System.out.println("B\t"+afp+"\t\t"+bpos);
+
 	}
-	
-	
+
 }
